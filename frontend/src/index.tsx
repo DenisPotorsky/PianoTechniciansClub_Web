03381658d7fdfbc30be88ce1454f25028930549f
DenisPotorsky <denis-s2@yandex.ru>
2026-09-02 23:07:58 +0500

    feat: миграция CRA → Vite, ускорение сборки в 10 раз

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import '@fontsource/dancing-script/400.css';
 import '@fontsource/dancing-script/700.css';
 
@@ -16,6 +15,4 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
